b56b4ad41b6c97e8fddd846281009d55166d96c0
takumasano <21hm038h@rikkyo.ac.jp>
2023-10-29 15:56:10 +0900

    difficult to figure out module

---
+++ PRJ_task1/sample.py
@@ -0,0 +1,5 @@
+import scipy.io
+import matplotlib.pyplot as plt
+
+# Load the .mat file (replace 'filename.mat' with your actual file name)
+data = scipy.io.loadmat('samples/sample data/1_19-Oct-2019_10-18-23_mouse.mat')
