e04abfc82a499955498c209e49dd3db8cdda612c
takumasano <21hm038h@rikkyo.ac.jp>
2023-11-11 17:13:32 +0900

    Organize folders

--- PRJ_task1/sample.py
+++
@@ -1,5 +0,0 @@
-import scipy.io
-import matplotlib.pyplot as plt
-
-# Load the .mat file (replace 'filename.mat' with your actual file name)
-data = scipy.io.loadmat('samples/sample data/1_19-Oct-2019_10-18-23_mouse.mat')
